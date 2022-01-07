e1880ddcde5c20a435db9d86747778f3dcc8c993
IasminSaba <iasminsaba4@gmail.com>
2022-01-07 16:00:18 -0300

    Adicionando Valid em update e create no AlunoController

--- src/main/java/com/osf/digital/alunos/alunos/controller/AlunoController.java
+++ src/main/java/com/osf/digital/alunos/alunos/controller/AlunoController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.transaction.Transactional;
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -45,7 +46,7 @@ public class AlunoController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity<AlunoDTO> create(@RequestBody AlunoForm form, UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<AlunoDTO> create(@RequestBody @Valid AlunoForm form, UriComponentsBuilder uriBuilder) {
         Aluno aluno = alunoService.create(form.getNome(), form.getIdade(), form.getSerie());
 
         URI uri = uriBuilder.path("/alunos/{id}").buildAndExpand(aluno.getId()).toUri();
@@ -57,7 +58,7 @@ public class AlunoController {
 
     @PutMapping("/{id}")
     @Transactional
-    public ResponseEntity<AlunoDTO> update(@PathVariable Long id, @RequestBody AlunoForm form) {
+    public ResponseEntity<AlunoDTO> update(@PathVariable Long id, @RequestBody @Valid AlunoForm form) {
         Optional<Aluno> optional = alunoService.findById(id);
         if (optional.isPresent()) {
             Aluno aluno = alunoService.update(id, form.getNome(), form.getIdade(), form.getSerie());
